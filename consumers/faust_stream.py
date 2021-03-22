e882631522ae8ff46bba82a70353a2a8779f6df4
Francesco Furlan <francesco.furlan@molo17.com>
2021-03-22 22:29:23 +0100

    feat: implement faust stream

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -3,9 +3,7 @@ import logging
 
 import faust
 
-
 logger = logging.getLogger(__name__)
-
 
 # Faust will ingest records from Kafka in this format
 class Station(faust.Record):
@@ -28,31 +26,48 @@ class TransformedStation(faust.Record):
     order: int
     line: str
 
+app = faust.App("stations-stream", broker = "kafka://kafka0:19092", store = "memory://")
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
-app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+topic = app.topic(
+    "jdbc.stations",
+    value_type = Station
+)
 
+out_topic = app.topic(
+    "com.udacity.stations.faust",
+    value_type = TransformedStation,
+    partitions = 1
+)
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+table = app.Table(
+   "com.udacity.stations.faust",
+   default = TransformedStation,
+   partitions = 1,
+   changelog_topic = out_topic,
+)
 
+def get_color(event):
+    assert isinstance(event, Station), "Event is not of Station type"
+    if event.red:
+        return "red"
+    elif event.green:
+        return "green"
+    elif event.blue:
+        return "blue"
+    else:
+        logger.warning(f"No color found for station {event.station_id}")
+        return None
+
+@app.agent(topic)
+async def station_event(events):
+    async for event in events:
+        transformed_station = TransformedStation(
+            station_id = event.station_id,
+            station_name = event.station_name,
+            order = event.order,
+            line = get_color(event)
+        )
+        table[event.station_id] = transformed_station
 
 if __name__ == "__main__":
     app.main()
